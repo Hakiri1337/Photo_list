a7e78b27c227656b6d7ffe1c27e1286a96d4cdc1
Kuba <kubawojewodzki46@gmail.com>
2022-05-08 17:00:14 +0200

    refracotr

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ImgBox from "./components/ImgBox";
 import { useEffect, useState } from "react";
 function App() {
   const [imagesData, setImagesData] = useState([]);
-
   const [imagesCount, setImagesCount] = useState(3);
   let url = "https://picsum.photos/v2/list";
   const getData = () => {
@@ -19,13 +18,13 @@ function App() {
   useEffect(() => {
     getData();
     // eslint-disable-next-line
-  }, [renderMoreImages]);
+  }, []);
 
   return (
     <div className="container">
       <button onClick={renderMoreImages}>Next</button>
       <div className="App">
-        {imagesData.splice(0, imagesCount).map((item) => (
+        {imagesData.slice(0, imagesCount).map((item) => (
           <ImgBox item={item} key={item.id} />
         ))}
       </div>
